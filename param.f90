6e73a77c03b389843bafa932f1524f3eeed4b2eb
stephan <stephansmit@hotmail.com>
2019-10-11 12:00:41 +0200

    added

--- param.f90
+++ param.f90
@@ -12,7 +12,7 @@ module mod_param
   integer   Mt,Nx,Mx,Nt 
   parameter (CFL               = 100.0)      ! CFL number / time step
      
-  parameter (systemSolve       = 3)        ! 1: pipe, 2: channel 3: boundary layer ! dpdz now defined in mkgrid should be 1 for channel/BL, 4 for the pipe
+  parameter (systemSolve       = 2)        ! 1: pipe, 2: channel 3: boundary layer ! dpdz now defined in mkgrid should be 1 for channel/BL, 4 for the pipe
 
   parameter (imax              = 96)       ! radial direction
   parameter (i1                = imax + 1)
@@ -23,7 +23,7 @@ module mod_param
   parameter (iwork             = 1)
   parameter (isave             = 1)
 
-  parameter (nTab              = 2500)    ! 2000 for co2, 2500 for pH2
+  parameter (nTab              = 2000)    ! 2000 for co2, 2500 for pH2
   parameter (nstep             = 100000)
 
   parameter (LoD               = 30)      ! pH2= 30
