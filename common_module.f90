d4577d5d08564cb5e12698564a353d06b9e0e3c6
stephan <stephansmit@hotmail.com>
2019-10-14 18:24:55 +0200

    added the module

--- common_module.f90
+++ common_module.f90
@@ -1,6 +1,6 @@
 module mod_common2
   use mod_turbmodels
   use mod_eosmodels
-  class(EOSModel), allocatable :: eos_model
+  class(EOSModel),  allocatable :: eos_model
   class(TurbModel), allocatable :: turb_model
 end module
