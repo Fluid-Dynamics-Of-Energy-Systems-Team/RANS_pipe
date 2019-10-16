0b5ce1d23a11c1a1a7f4da83ef75889e6617c21b
stephan <stephansmit@hotmail.com>
2019-10-16 17:07:49 +0200

    added the validation notebook, just started

--- param.f90
+++ param.f90
@@ -12,7 +12,7 @@ module mod_param
   integer   Mt,Nx,Mx,Nt 
 
   parameter (CFL               = 100.0)   ! CFL number / time step
-  parameter (systemSolve       = 2)       ! 1: pipe, 2: channel 3: boundary layer !**dpdz now defined in mkgrid should be 1 for channel/BL, 4 for the pipe**!!
+  parameter (systemSolve       = 1)       ! 1: pipe, 2: channel 3: boundary layer !**dpdz now defined in mkgrid should be 1 for channel/BL, 4 for the pipe**!!
   parameter (imax              = 96)      ! radial direction
   parameter (i1                = imax + 1)
   parameter (K_start_heat      = 3)       ! for isoflux:    after home many cells in x will you start heating
@@ -33,7 +33,7 @@ module mod_param
   parameter (Fr_1              =  0.0)
   parameter (EOSmode           = 0)       ! 0..IG, 1..SCCO2/sPH2
   parameter (select_init       = 0)       ! 0..std initialization, 1..read inflow , 2..read restart file
-  parameter (turbmod           = 3)       ! 0..laminar, 1..SA, 2..MK, 3..V2F, 4..SST
+  parameter (turbmod           = 2)       ! 0..laminar, 1..SA, 2..MK, 3..V2F, 4..SST
   parameter (ctheta            = 0.3)
   parameter (modVF             = 0)       ! 0..Original 1..LienKalitzin Time/Length scale VF model
   parameter (alphac            = 0.1)
